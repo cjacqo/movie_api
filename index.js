2c79a506c135fcd4f498395b7cdf377a1b306897
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-05-31 08:56:27 -0700

    Installed and implemented CORS, installed and setup express-validator, updated /users POST and PUT requests for validation, setup start script in package.json, updated app.listen function

--- index.js
+++ index.js
@@ -18,9 +18,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: true }))
 
+const cors = require('cors')
+let allowedOrigins = ['https://localhost:8080']
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin
+      return callback(new Error(message), false)
+    }
+    return callback(null, true)
+  }
+}))
+
 let auth = require('./auth.js')(app)
 const passport = require('passport')
 require('./passport.js')
+
+const { check, validationResult } = require('express-validator')
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API for movies!')
@@ -55,54 +70,92 @@ app.get('/users/:UserName', passport.authenticate('jwt', { session: false }), (r
 
 // CREATE - Mongoose
 // - a user
-app.post('/users', (req, res) => {
-  let hashedPassword = Users.hashPassword(req.body.Password)
-  Users.findOne({ UserName: req.body.UserName })
+app.post('/users',
+  // Validation logic here for request
+  // you can either use a chain of methods like .not().isEmpty()
+  // which means "opposite of isEmpty" in plain english "is not empty"
+  // or use .isLength({ min: 5 }) which means
+  // minimum value of 5 characters are only allowed
+  [
+    check('UserName', 'Username is required').isLength({ min: 5 }),
+    check('UserName', 'Username contains non alphanumeric characters - not allowed').isAlphanumeric(),
+    check('Password', 'Password must be between 8 and 20 characters').isLength({ min: 5, max: 20 }),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ],
+  (req, res) => {
+    let errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password)
+    Users.findOne({ UserName: req.body.UserName })
+      .then(user => {
+        if (user) {
+          return res.status(400).send(req.body.UserName + ' already exists')
+        } else {
+          Users.create({
+            FirstName: req.body.FirstName,
+            LastName: req.body.LastName,
+            UserName: req.body.UserName,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            DOB: req.body.DOB
+          })
+          .then(user => { res.status(201).json(user) })
+          .catch(err => {
+            console.error(err)
+            console.log(req.body.FirstName)
+            res.status(500).send('Error: ' + err)
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send('Error: ' + err)
+      })
+  }
+)
+
+// UPDATE - Mongoos
+// - user by username
+app.put('/users/:UserName',
+  passport.authenticate('jwt', { session: false }),
+  // Validation logic here for request
+  // *** MAKE SURE VALIDATION ONLY APPLIES TO REQUESTS/FIELDS FILLED
+  [
+    check('UserName', 'Username contains non alphanumeric characters - not allowed').isAlphanumeric(),
+    check('UserName', 'Username is required').isLength({ min: 5 }),
+    check('Password', 'Password must be between 8 and 20 characters').isLength({ min: 5, max: 20 }),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ],
+  (req, res) => {
+    let errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() })
+    }
+
+    Users.findOneAndUpdate({ UserName: req.params.UserName },
+    {
+      UserName: req.body.UserName,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      DOB: req.body.DOB
+    },
+    { new: true }) // make sure that the updated document is retured
     .then(user => {
-      if (user) {
-        return res.status(400).send(req.body.UserName + ' already exists')
-      } else {
-        Users.create({
-          FirstName: req.body.FirstName,
-          LastName: req.body.LastName,
-          UserName: req.body.UserName,
-          Password: hashedPassword,
-          Email: req.body.Email,
-          DOB: req.body.DOB
-        })
-        .then(user => { res.status(201).json(user) })
-        .catch(err => {
-          console.error(err)
-          console.log(req.body.FirstName)
-          res.status(500).send('Error: ' + err)
-        })
-      }
+      res.status(200).send(`Successfully updated ${user.UserName}`)
     })
     .catch(err => {
       console.error(err)
       res.status(500).send('Error: ' + err)
     })
-})
-
-// UPDATE - Mongoos
-// - user by username
-app.put('/users/:UserName', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOneAndUpdate({ UserName: req.params.UserName },
-  {
-    UserName: req.body.UserName,
-    Password: req.body.Password,
-    Email: req.body.Email,
-    DOB: req.body.DOB
-  },
-  { new: true }) // make sure that the updated document is retured
-  .then(user => {
-    res.status(200).send(`Successfully updated ${user.UserName}`)
-  })
-  .catch(err => {
-    console.error(err)
-    res.status(500).send('Error: ' + err)
-  })
-})
+  }
+)
 
 // UPDATE - Mongoose
 // - add a movie to user's list of favorites
@@ -241,6 +294,12 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke! ' + err)
 })
 
-app.listen(8080, () => {
-  console.log('App is running on port 8080')
+const port = process.env.PORT || 8080
+
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port: ' + port)
 })
+
+// app.listen(8080, () => {
+//   console.log('App is running on port 8080')
+// })

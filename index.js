576458550e11719c61b286e56b64681895402257
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-07-03 15:41:19 -0700

    Make path to documentation absolute

--- index.js
+++ index.js
@@ -289,7 +289,7 @@ app.get('/directors/:Name', passport.authenticate('jwt', { session: false }), (r
 
 // READ
 app.get('/documentation', (req, res) => {
-  res.sendFile('documentation.html')
+  res.sendFile('/documentation.html')
 })
 
 app.use((err, req, res, next) => {

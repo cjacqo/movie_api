faf1f06bb40717365ee6a70f260468c1d65eeb6b
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-09-21 23:07:52 -0700

    updated route for deleting a favorite movie from users list

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ app.delete(
    { new: true }
   )
    .then((user) => {
-    res.status(200).send("Movie has been removed from list of favorites");
+    res.status(200).json(user.FavoriteMovies);
    })
    .catch((err) => {
     console.error(err);
